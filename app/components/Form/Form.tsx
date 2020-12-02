4acacab392f0606b27d94a57288a7761df7359d7
Srdjan Jovic <Srdjan.Jovic@rt-rk.com>
2020-12-02 15:13:56 +0100

    Add component module, include component to App
    - creatre component file structure
    - create and export placeholder Form, FormContext and FormInput functions

---
+++ app/components/Form/Form.tsx
@@ -0,0 +1,13 @@
+import {useState, useContext, useEffect} from 'react';
+
+export const FormContext = (props) => {
+
+};
+
+export const Form = (props) => {
+
+};
+
+export const FormInput = (props) => {
+
+}

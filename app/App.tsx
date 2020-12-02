e19ff999c1bc8055fab0ceeb954d07ee88f7a7cb
Srdjan Jovic <Srdjan.Jovic@rt-rk.com>
2020-12-02 15:20:31 +0100

    Fix project structure and init file
    - add new App.tsx file
    - move PageWithForm component to component folder and render it inside App component

--- app/App.tsx
+++ app/App.tsx
@@ -1,48 +1,8 @@
-/*
-	Napraviti mini-library za form submission.
-	Implementacija u pozadini treba da koristi context API i da se ne oslanja na postojece npm pakete za forme.
-	Sva logika (input[value], input[onChange], form[onSumbit]) treba da se nalazi u FormContext, Form i FormInput
-	komponentama tako da nije izlozena korisniku library-a.
+import React from 'react';
+import {PageWithForm} from './components/PageWithForm';
 
-	State forme moze da se nalazi u FormContext ili u Form komponenti
-	Ukoliko je sva logika u Form, FormContext nije ni potreban tako da ga mozes izbaciti.
-
-	Ispod je primjer komponente koja bi koristila library na zeljeni nacin. Ukoliko ovakva struktura
-	bude u browseru rezultovala renderovanju forme koja na submit loguje userInfo objekat sa izmjenjenim vrijednostima,
-	zadatak se smatra uspjesno zavrsenim.
-
-	Koristiti React i TypeScript i parcel.
-	Pozeljno je napisati testove za library.
-
-	Puno srece ;-)
-*/
-import React, { useState } from 'react';
-import {Form, FormContext, FormInput} from './components/Form/Form';
-
-export const PageWithForm = () => {
-	// Example
-	const [userInfo, setUserInfo] = useState({
-		email: 'example@alea.com',
-		age: 30,
-		name: 'John Doe',
-		phone: {
-			ext: '00387',
-			number: '65/123-456',
-		},
-	});
-
-	return (
-		<div>
-			<FormContext note="Ovo je opcionalan wrapper">
-				<Form initialValues={userInfo} onSubmit={setUserInfo}>
-					<FormInput type="email" required name="email" placeHolder="your@email.com" />
-					<FormInput type="number" name="age" />
-					<FormInput type="text" required name="name" />
-					<FormInput type="text" name="phone.ext" />
-					<FormInput type="text" name="phone.number" />
-					<FormInput type="submit" value="Submit" />
-				</Form>
-			</FormContext>
-		</div>
-	);
+export const App = () => {
+    return (
+        <PageWithForm></PageWithForm>
+    )
 };
